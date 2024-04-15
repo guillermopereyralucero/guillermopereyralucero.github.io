bc3df66f15c127018dca9e3bb2d5eac26d793594
Guillermo Pereyra Lucero <99100769+guillermopereyralucero@users.noreply.github.com>
2024-04-15 12:54:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const btnElement = document.querySelector('.js-btn');
+console.log('Hola mundo');
 btnElement.addEventListener('click',()=>{console.log('Hola mundo');});
 btnElement.addEventListener('mouseover',()=>{console.log('Hola mouse');});
